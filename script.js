78bd59c96814ac69ff4021e333907d7e7657e175
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-06 14:45:34 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Rest params (...) permite representar um número indefinido de argumentos como um array.
 
+function values(...rest) {
+  //pedemos mudar a nomenclaturatambém se quisermos. "args" é muito comum de se usar.
+  //console.log(a);
 
+  console.log(rest.length); //exemplo
+  console.log(...rest);
+  //o rest retorna um array, se tirar os tres pontos do console.log(...rest) dá pra entender.
+  console.log(rest);
+}
+values(2, 1, 3, 4, 5);
 
-
+//finalizando
